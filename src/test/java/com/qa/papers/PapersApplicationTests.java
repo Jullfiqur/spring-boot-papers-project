7695961cbc56d34f121d083fe28a3e6719974cde
Jullfiqur Rahman <JRahman@qa.com>
2020-09-03 22:25:47 +0100

    Initial commit

---
+++ src/test/java/com/qa/papers/PapersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.papers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
