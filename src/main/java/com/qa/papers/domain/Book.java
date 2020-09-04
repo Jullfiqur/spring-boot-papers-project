45a60a56edcc3eed7859a1087889eb0b9fe3fbfb
Jullfiqur Rahman <JRahman@qa.com>
2020-09-04 10:17:27 +0100

    Added the Book entity class

---
+++ src/main/java/com/qa/papers/domain/Book.java
@@ -0,0 +1,54 @@
+package com.qa.papers.domain;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+   @Id
+   @GeneratedValue
+    private Long id;
+
+   @Column
+    private String title;
+
+   @Column
+    private String description;
+
+    public Book() {
+    }
+
+    public Book(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
